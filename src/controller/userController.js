de8afff1fb9599fb01e23fe5eec065c0995e9920
ALEX FRANCIS BANNACH <alex_bannach@estudante.sc.senai.br>
2023-09-28 21:39:51 -0300

    Delete

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -30,6 +30,8 @@ async function GetAllUsers(req, res){
 
         const [users] = await connection.query('SELECT * FROM user');
 
+        await connection.end();
+
         res.status(200).json(users);
         
     } catch (error) {
@@ -43,7 +45,79 @@ async function GetAllUsers(req, res){
     }
 }
 
+async function GetUserById(req, res){
+    const { id } = req.params;
+
+    try {
+        const connection = await mysql.createConnection(databaseConfig);
+
+        const [user] = await connection.query('SELECT * FROM user WHERE id = ?', [id]);
+
+        await connection.end();
+
+        res.status(200).json(user);
+
+    } catch (error) {
+        res.status(500).send(
+            {
+                message: 'Error getting user!',
+                body: error.message,
+            }
+        )
+    }
+}
+
+async function UpdateUser(req, res){
+    const { id } = req.params;
+    const { name, password } = req.query;
+    
+    try {
+        const connection = await mysql.createConnection(databaseConfig);
+
+        await connection.query('UPDATE user SET name = ?, password = ? WHERE id = ?', [name, password, id]);
+
+        await connection.end();
+
+        res.status(200).send("Update success!");
+
+    } catch (error) {
+        res.status(500).send(
+            {
+                message: 'Error updating user!',
+                body: error.message,
+            }
+        )
+    }
+
+}
+
+async function DeleteUser(req, res){
+    const { id } = req.params;
+
+    try {
+        const connection = await mysql.createConnection(databaseConfig);
+
+        await connection.query('DELETE FROM user WHERE id = ?', [id]);
+
+        await connection.end();
+
+        res.status(200).send("Delete success!");
+
+    } catch (error) {
+        res.status(500).send(
+            {
+                message: 'Error deleting user!',
+                body: error.message,
+            }
+        )
+    }
+}
+
+
 module.exports = {
     CreateUser,
     GetAllUsers,
+    GetUserById,
+    UpdateUser,
+    DeleteUser
 }
--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -4,5 +4,8 @@ const userController = require('../controller/userController');
 
 router.post('/users', userController.CreateUser);
 router.get('/users',userController.GetAllUsers);
+router.get('/users/:id', userController.GetUserById);
+router.put('/users/:id', userController.UpdateUser);
+router.delete('/users/:id', userController.DeleteUser);
 
 module.exports = router;
